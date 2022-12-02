3b5bd5f6a1f6310a7bc5308aacf55ffa8b68fb98
unknown <iury.flores@gmail.com>
2022-12-01 21:39:47 -0300

    Create payment routes (get paid parcels, remove payday, edit parcel, pay parcel)

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -23,15 +23,70 @@ router.get("/payments/paid", async (req, res, next) => {
   }
 });
 //Pay a parcel
-router.put("/payment/:id", async (req, res, next) => {
+router.put("/payment/pay/:id", async (req, res, next) => {
   const { id } = req.params;
 
   try {
     const foundPayment = await Payment.findById(id);
-    res.status(200).json(foundPayment);
+
+    if (!foundPayment) {
+      return res.status(404).json({ msg: "Payment not found!" });
+    }
+
+    const today = new Date();
+
+    const paid = await Payment.findByIdAndUpdate(
+      id,
+      { payDay: today },
+      { new: true }
+    );
+
+    res.status(200).json(paid);
   } catch (error) {
     next(error);
   }
 });
 
+//Edit payment
+router.put("/payment/edit/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const { price, dueDate, payDay } = req.body;
+
+  try {
+    const foundPayment = await Payment.findById(id);
+
+    if (!foundPayment) {
+      return res.status(404).json({ msg: "Payment not found!" });
+    }
+
+    const newPayment = await Payment.findByIdAndUpdate(
+      id,
+      { ...req.body },
+      { new: true }
+    );
+
+    res.status(200).json(newPayment);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//Edit remove payday
+router.delete("/payment/:id", async (req, res, next) => {
+  const { id } = req.params;
+
+
+  try {
+    const foundPayment = await Payment.findById(id)
+    if(!foundPayment){
+        return res.status(404).json({msg:"Payment not found!"})
+    }
+    const payDayDeleted = await Payment.findByIdAndUpdate(
+      id,
+      { payDay: null },
+      { new: true }
+    );
+    res.status(200).json(payDayDeleted);
+  } catch (error) {}
+});
 export default router;
