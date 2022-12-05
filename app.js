054aa47692ae4630e997a5f030f3d3e3dc15ac72
unknown <iury.flores@gmail.com>
2022-12-05 20:00:38 -0300

    Fix:auth routes server

--- app.js
+++ app.js
@@ -35,5 +35,5 @@ app.use("/", paymentRoutes);
 handleError(app);
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+    console.log(`Server running on port ${process.env.PORT}`);
 });
