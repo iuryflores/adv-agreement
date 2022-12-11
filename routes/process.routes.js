4dafad43c91150e7695948bee330a4cdc5114716
unknown <iury.flores@gmail.com>
2022-12-10 21:02:32 -0300

    Feat: populating defendantId in process get

--- routes/process.routes.js
+++ routes/process.routes.js
@@ -18,7 +18,7 @@ router.get("/process", async (req, res, next) => {
 router.get("/process/:id", async (req, res, next) => {
   const { id } = req.params;
   try {
-    const processFound = await Process.findById(id);
+    const processFound = await Process.findById(id).populate('defendantId');
     return res.status(200).json(processFound);
   } catch (error) {
     next(error);
